0c7fd2a3ffe68d3f9f36c7d87be087d2c0c014d9
Siva Gollapalli <sivagollapalli88@gmail.com>
2021-08-06 17:53:49 +0530

    Added following set of functions
    - descendant_of
    - descendant_or_self_of
    - child_of
    - child_or_self_of
    - ancestor_of
    - ancestor_or_self_of
    - parent_of
    - parent_or_self_of

--- lib/snomed_query.rb
+++ lib/snomed_query.rb
@@ -14,24 +14,63 @@ module SnomedQuery
     end
   end
 
+  def details(code)
+    build_uri_and_send(code)
+  end
+
+  def descendant_of(code)
+    build_uri_and_send("<#{code}")
+  end
+
+  def descendant_or_self_of(code)
+    build_uri_and_send("<<#{code}")
+  end
+
+  def child_of(code)
+    build_uri_and_send("<!#{code}")
+  end
+
+  def child_or_self_of(code)
+    build_uri_and_send("<<!#{code}")
+  end
+
+  def ancestor_of(code)
+    build_uri_and_send(">#{code}")
+  end
+
+  def ancestor_or_self_of(code)
+    build_uri_and_send(">>#{code}")
+  end
+
+  def parent_of(code)
+    build_uri_and_send(">!#{code}")
+  end
+
+  def parent_or_self_of(code)
+    build_uri_and_send(">>!#{code}")
+  end
+
+  private
+
+  def build_uri_and_send(ecl_query)
+    uri = build_fhir_uri(ecl_query)
+    response = client.get(uri.request_uri)
+    handle_response(response)
+  end
+
   def client
-    @client ||= ::Faraday.new(
+    ::Faraday.new(
       url: ENV['SNOMED_SERVER_URL'],
       headers: { 'Content-Type' => 'application/json' }
     )
   end
 
-  def details(code)
-    uri = URI::HTTP.build(
+  def build_fhir_uri(ecl_query)
+    URI::HTTP.build(
       path: '/fhir/ValueSet/$expand', 
-      query: "url=#{snomed_variant}?fhir_vs=ecl/#{code}"
+      query: "url=#{snomed_variant}?fhir_vs=ecl/#{ecl_query}"
     )
-
-    response = client.get(uri.request_uri)
-    handle_response(response)
   end
-
-  private
 
   def handle_response(response)
     parsed_response = JSON.parse(response.body)
